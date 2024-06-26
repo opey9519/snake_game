8e9145b19e866c8abff2486bb5aebc2ed9568e2c
Gavin Wilson <gmanwilson@icloud.com>
2024-06-26 14:39:28 -0400

    Added randomized "Apple" spawner. On collision randomized location respawn.

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 # Description: Recreate popular "Snake Game"
 
 import pygame
+import random
 
 # Initializes the window
 pygame.init()
@@ -14,6 +15,8 @@ run = True
 # Position variables
 x1 = 300
 y1 = 300
+x_apple = random.randrange(0, 801)
+y_apple = random.randrange(0, 601)
 
 # Movement change variables
 x1_change = 0
@@ -47,11 +50,26 @@ while run:
     # Changes (x,y) coordinate based on key stroke
     x1 += x1_change
     y1 += y1_change
+    
+    # If snake hits boundaries game ends
+    if x1 <= 0 or x1 >= 800:
+        run = False
+    elif y1 <= 0 or y1 >= 600:
+        run = False
+    
     # Screen black on refresh
     screen.fill((0,0,0))
 
     # Creating rectangle with rgb (lime green)
-    pygame.draw.rect(screen, (0, 255, 0), [x1,y1,10,10])
+    snake = pygame.draw.rect(screen, (0, 255, 0), [x1,y1,20,20])
+    apple = pygame.draw.rect(screen, (255,0,0), [x_apple, y_apple, 20, 20])
+
+    # Collision statement (if snake collides with apple, respawn apple, append snake)
+    if pygame.Rect.colliderect(snake, apple):
+        x_apple = random.randrange(0, 801)
+        y_apple = random.randrange(0, 601)
+
+        apple = pygame.draw.rect(screen, (255,0,0), [x_apple, y_apple, 20, 20])
     
     # Updates the screen to allow next frame
     pygame.display.update()
