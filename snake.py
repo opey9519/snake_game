8ec1de9b0e7c9bd2bcd94a7ff677101315d13aa0
Gavin Wilson <gmanwilson@icloud.com>
2024-06-28 13:43:58 -0400

    Opposite directional movement prohibited Added additional movement functionality that prohibits movement into the opposite direction in which snake is traveling.

--- snake.py
+++ snake.py
@@ -24,10 +24,11 @@ snake_length = 1
 def plot_snake(screen, color, snake_list):
     for x, y in snake_list:
         pygame.draw.rect(screen, color, [x, y, 20, 20])
-
+        
 # Movement change variables
 x1_change = 0
 y1_change = 0
+direction = 'x' # Directional variable that prohibits opposite direcitonal movement
 
 clock = pygame.time.Clock()
 
@@ -41,18 +42,22 @@ while run:
 
         # On keydown press (LEFT, RIGHT, UP, DOWN), x1_change, y1_change variables are altered to allow continous movement during while loop
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT and direction != 'right':
                 x1_change = -10
                 y1_change = 0
-            elif event.key == pygame.K_RIGHT:
+                direction = 'left'
+            elif event.key == pygame.K_RIGHT and direction != 'left':
                 x1_change = 10
                 y1_change = 0
-            elif event.key == pygame.K_UP:
+                direction = 'right'
+            elif event.key == pygame.K_UP and direction != 'down':
                 y1_change = -10
                 x1_change = 0
-            elif event.key == pygame.K_DOWN:
+                direction = 'up'
+            elif event.key == pygame.K_DOWN and direction != 'up':
                 y1_change = 10
                 x1_change = 0
+                direction = 'down'
 
     # Changes (x,y) coordinate based on key stroke
     x1 += x1_change
@@ -89,12 +94,12 @@ while run:
         apple = pygame.draw.rect(screen, (255,0,0), [x_apple, y_apple, 20, 20])
 
     # Plots an instance of rectangle based on snake length (incremented by food collision)
-    snake_parts = plot_snake(screen, (0, 255, 0), snake_list)
+    plot_snake(screen, (0, 255, 0), snake_list)
     
     # Updates the screen to allow next frame
     pygame.display.update()
 
-    # Sets frames per second to 30
+    # Sets frames per second to 20
     clock.tick(20)
 
 pygame.quit() # Quits intializer
